7ad1fc788c24353e95c34ec5aa0d39725d058935
Sarah <sarah@sarahs-macbook-pro.local>
2021-12-02 21:53:35 +1300

    Initial Commit

---
+++ Journey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Journey
+//
+//  Created by Tejaswini on 2/12/21.
+//  Copyright © 2021 Tejaswini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
